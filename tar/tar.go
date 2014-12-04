f4ec54b25fd86bf18c5ec599ecc4cf8f414d3d96
Eric Snow <ericsnowcurrently@gmail.com>
2014-12-04 11:34:12 -0700

    Add ExtractArchivedFile.

--- tar/tar.go
+++ tar/tar.go
@@ -8,6 +8,7 @@ package tar
 
 import (
 	"archive/tar"
+	"compress/gzip"
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
@@ -40,6 +41,55 @@ func FindFile(tarFile io.Reader, filename string) (*tar.Header, io.Reader, error
 	}
 
 	return nil, nil, errors.NotFoundf(filename)
+}
+
+type archivedFile struct {
+	io.Reader
+	archive io.Closer
+	gzr     io.Closer
+}
+
+// ExtractArchivedFile extracts a file from within a gzipped tar file.
+// The returned io.ReadCloser assumes responsibility for closing the
+// given archive file. Likewise, if an error happens in
+// ExtractArchivedFile then the archive will be closed. If the file is
+// not found in the archive then errors.NotFound is returned.
+//
+// Note: for uncompressed tar files just use FindFile directly.
+func ExtractArchivedFile(archive io.ReadCloser, filename string) (io.ReadCloser, error) {
+	if filepath.IsAbs(filename) {
+		return nil, errors.Errorf("filename must be relative, got %q", filename)
+	}
+
+	gzr, err := gzip.NewReader(archive)
+	if err != nil {
+		// We're already handling an error so don't worry when closing.
+		archive.Close()
+		return nil, errors.Annotatef(err, "cannot unzip %q", filename)
+	}
+
+	_, file, err := FindFile(gzr, filename)
+	if err != nil {
+		// We're already handling an error so don't worry when closing.
+		gzr.Close()
+		archive.Close()
+		return nil, errors.Trace(err)
+	}
+
+	return &archivedFile{file, archive, gzr}, nil
+}
+
+// Close closes the underlying archive file and gzip reader.
+func (af *archivedFile) Close() error {
+	err1 := errors.Trace(af.gzr.Close())
+	err2 := errors.Trace(af.archive.Close())
+	if err1 != nil {
+		if err2 != nil {
+			return errors.Errorf("got multiple errors while closing: %v, %v", err1, err2)
+		}
+		return errors.Trace(err1)
+	}
+	return errors.Trace(err2)
 }
 
 // TarFiles writes a tar stream into target holding the files listed
