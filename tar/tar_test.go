ef1c56772ae40e294eaa1e17c602dcba833ef3a9
Eric Snow <ericsnowcurrently@gmail.com>
2016-01-21 11:33:05 -0700

    Manage test files better.

--- tar/tar_test.go
+++ tar/tar_test.go
@@ -34,53 +34,19 @@ func (t *TarSuite) SetUpTest(c *gc.C) {
 	t.IsolationSuite.SetUpTest(c)
 }
 
+func (t *TarSuite) TearDownTest(c *gc.C) {
+	removeTestFiles(c, t.testFiles)
+
+	t.IsolationSuite.TearDownTest(c)
+}
+
 func (t *TarSuite) createTestFiles(c *gc.C) {
-	tarDirE := filepath.Join(t.cwd, "TarDirectoryEmpty")
-	err := os.Mkdir(tarDirE, os.FileMode(0755))
-	c.Check(err, gc.IsNil)
-
-	tarDirP := filepath.Join(t.cwd, "TarDirectoryPopulated")
-	err = os.Mkdir(tarDirP, os.FileMode(0755))
-	c.Check(err, gc.IsNil)
-
-	tarlink1 := filepath.Join(t.cwd, "TarLink")
-	err = os.Symlink(tarDirP, tarlink1)
-	c.Check(err, gc.IsNil)
-
-	tarSubFile1 := filepath.Join(tarDirP, "TarSubFile1")
-	tarSubFile1Handle, err := os.Create(tarSubFile1)
-	c.Check(err, gc.IsNil)
-	tarSubFile1Handle.WriteString("TarSubFile1")
-	tarSubFile1Handle.Close()
-
-	tarSublink1 := filepath.Join(tarDirP, "TarSubLink")
-	err = os.Symlink(tarSubFile1, tarSublink1)
-	c.Check(err, gc.IsNil)
-
-	tarSubDir := filepath.Join(tarDirP, "TarDirectoryPopulatedSubDirectory")
-	err = os.Mkdir(tarSubDir, os.FileMode(0755))
-	c.Check(err, gc.IsNil)
-
-	tarFile1 := filepath.Join(t.cwd, "TarFile1")
-	tarFile1Handle, err := os.Create(tarFile1)
-	c.Check(err, gc.IsNil)
-	tarFile1Handle.WriteString("TarFile1")
-	tarFile1Handle.Close()
-
-	tarFile2 := filepath.Join(t.cwd, "TarFile2")
-	tarFile2Handle, err := os.Create(tarFile2)
-	c.Check(err, gc.IsNil)
-	tarFile2Handle.WriteString("TarFile2")
-	tarFile2Handle.Close()
-	t.testFiles = []string{tarDirE, tarDirP, tarlink1, tarFile1, tarFile2}
-
+	t.testFiles = createTestFiles(c, t.cwd)
 }
 
 func (t *TarSuite) removeTestFiles(c *gc.C) {
-	for _, removable := range t.testFiles {
-		err := os.RemoveAll(removable)
-		c.Assert(err, gc.IsNil)
-	}
+	removeTestFiles(c, t.testFiles)
+	t.testFiles = nil
 }
 
 type expectedTarContents struct {
@@ -197,7 +163,6 @@ func (t *TarSuite) TestTarFiles(c *gc.C) {
 	outputBytes := outputTar.Bytes()
 	fileShaSum := shaSumFile(c, bytes.NewBuffer(outputBytes))
 	c.Assert(shaSum, gc.Equals, fileShaSum)
-	t.removeTestFiles(c)
 	t.assertTarContents(c, testExpectedTarContents, bytes.NewBuffer(outputBytes))
 }
 
@@ -241,7 +206,7 @@ func (t *TarSuite) TestUnTarFilesUncompressed(c *gc.C) {
 	trimPath := fmt.Sprintf("%s/", t.cwd)
 	_, err := tarutil.TarFiles(t.testFiles, &outputTar, trimPath)
 	c.Check(err, gc.IsNil)
-	t.removeTestFiles(c)
+	t.removeTestFiles(c) // ...not strictly necessary.
 
 	outputDir := filepath.Join(t.cwd, "TarOuputFolder")
 	err = os.Mkdir(outputDir, os.FileMode(0755))
@@ -257,7 +222,7 @@ func (t *TarSuite) TestFindFileFound(c *gc.C) {
 	trimPath := fmt.Sprintf("%s/", t.cwd)
 	_, err := tarutil.TarFiles(t.testFiles, &outputTar, trimPath)
 	c.Assert(err, gc.IsNil)
-	t.removeTestFiles(c)
+	t.removeTestFiles(c) // ...not strictly necessary.
 
 	_, file, err := tarutil.FindFile(&outputTar, "TarDirectoryPopulated/TarSubFile1")
 	c.Assert(err, gc.IsNil)
@@ -274,7 +239,7 @@ func (t *TarSuite) TestFindFileNotFound(c *gc.C) {
 	trimPath := fmt.Sprintf("%s/", t.cwd)
 	_, err := tarutil.TarFiles(t.testFiles, &outputTar, trimPath)
 	c.Assert(err, gc.IsNil)
-	t.removeTestFiles(c)
+	t.removeTestFiles(c) // ...not strictly necessary.
 
 	_, _, err = tarutil.FindFile(&outputTar, "does_not_exist")
 
@@ -300,4 +265,59 @@ func (t *TarSuite) TestUntarFilesHeadersIgnored(c *gc.C) {
 		return err
 	})
 	c.Assert(err, gc.IsNil)
+}
+
+func createTestFiles(c *gc.C, cwd string) []string {
+	tarDirE := filepath.Join(cwd, "TarDirectoryEmpty")
+	err := os.Mkdir(tarDirE, os.FileMode(0755))
+	c.Check(err, gc.IsNil)
+
+	tarDirP := filepath.Join(cwd, "TarDirectoryPopulated")
+	err = os.Mkdir(tarDirP, os.FileMode(0755))
+	c.Check(err, gc.IsNil)
+
+	tarlink1 := filepath.Join(cwd, "TarLink")
+	err = os.Symlink(tarDirP, tarlink1)
+	c.Check(err, gc.IsNil)
+
+	tarSubFile1 := filepath.Join(tarDirP, "TarSubFile1")
+	tarSubFile1Handle, err := os.Create(tarSubFile1)
+	c.Check(err, gc.IsNil)
+	tarSubFile1Handle.WriteString("TarSubFile1")
+	tarSubFile1Handle.Close()
+
+	tarSublink1 := filepath.Join(tarDirP, "TarSubLink")
+	err = os.Symlink(tarSubFile1, tarSublink1)
+	c.Check(err, gc.IsNil)
+
+	tarSubDir := filepath.Join(tarDirP, "TarDirectoryPopulatedSubDirectory")
+	err = os.Mkdir(tarSubDir, os.FileMode(0755))
+	c.Check(err, gc.IsNil)
+
+	tarFile1 := filepath.Join(cwd, "TarFile1")
+	tarFile1Handle, err := os.Create(tarFile1)
+	c.Check(err, gc.IsNil)
+	tarFile1Handle.WriteString("TarFile1")
+	tarFile1Handle.Close()
+
+	tarFile2 := filepath.Join(cwd, "TarFile2")
+	tarFile2Handle, err := os.Create(tarFile2)
+	c.Check(err, gc.IsNil)
+	tarFile2Handle.WriteString("TarFile2")
+	tarFile2Handle.Close()
+
+	return []string{
+		tarDirE,
+		tarDirP,
+		tarlink1,
+		tarFile1,
+		tarFile2,
+	}
+}
+
+func removeTestFiles(c *gc.C, testFiles []string) {
+	for _, removable := range testFiles {
+		err := os.RemoveAll(removable)
+		c.Assert(err, gc.IsNil)
+	}
 }
